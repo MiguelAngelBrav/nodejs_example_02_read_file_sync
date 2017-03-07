1f8cad16d65d579757d646050b7dac8bf9f5deaa
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2017-03-07 17:34:38 -0300

    first commit

---
+++ read_file.js
@@ -0,0 +1,10 @@
+var http = require("http");
+var fs = require("fs");
+
+var html = fs.readFileSync("./index.html");
+
+http.createServer(function(req, res){
+  console.log("createServer!!");
+  res.write(html);
+  res.end();
+}).listen(8090);
